2adc4008cbb4bb16166ff4aa9b0cc5fb63edd6cc
cchechang <cchechang@mastercontrol.com>
2019-12-17 18:20:48 -0700

    Set up mongo db connection and able to save post to the database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Post = require('./models/post');
 const app = express();
+
+mongoose.connect('mongodb+srv://chacha:qIamU68qPaGliZd4@cluster0-m4e7f.mongodb.net/node-angular?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then(() => {
+      console.log('Connected to database!');
+    }
+  )
+  .catch(() => {
+    console.log('Connection failed');
+
+  })
+
 
 app.use(bodyParser.json());
 
@@ -14,9 +30,11 @@ app.use((req,res,next) => {
 })
 
 app.post('/api/posts', (req,res,next) => {
-  const post = req.body;
-  console.log(post);
-
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save();
   res.status(200).json({
     message: 'Post added!'
   })
