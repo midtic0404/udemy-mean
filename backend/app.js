24938e06741b4cbf133c78289f26444929f8233c
Chia Che Chang <midtic0404@gmail.com>
2019-12-24 16:17:05 -0700

    Added update post logic

--- backend/app.js
+++ backend/app.js
@@ -56,6 +56,31 @@ app.get('/api/posts', (req,res,next) => {
   )
 });
 
+app.get('/api/posts/:id', (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if(post){
+      res.status(200).json(post);
+    }else {
+      res.status(404).json({message: 'Post not found'});
+    }
+  })
+});
+
+app.put('/api/posts/:id', (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  })
+
+  Post.updateOne({_id: req.params.id}, post).then(result => {
+    res.status(200).json({
+      message: "Update successful!"
+    });
+
+  })
+});
+
 app.delete('/api/posts/:id', (req, res, next) => {
   Post.deleteOne({
     _id: req.params.id
